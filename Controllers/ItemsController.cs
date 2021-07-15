5563737c6a514aaad05d37952dec46d9ed9cdd55
TheWolverineCoder <armageddon16@abv.bg>
2021-07-15 13:08:11 +0300

    init commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers{
+    //Get/items
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase{
+        private readonly InMemItemsRepository repository;
+        public ItemsController(){
+            repository = new InMemItemsRepository();
+        }
+
+        //GET/items
+        [HttpGet]
+        public IEnumerable<Item> GetItems(){
+            var items = repository.GetItems();
+            return items;
+        }
+
+        //GET/items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id){
+            var item = repository.GetItem(id);
+            if(item is null){
+                return NotFound();
+            }
+            return item;
+        }
+    
+    }
+}
