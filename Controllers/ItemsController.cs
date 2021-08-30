a35c689a61451d5d8bed0650e5395ed1d621e3b6
Пламен Господинов <plamko3@abv.bg>
2021-08-30 19:34:41 +0300

    Created MongoDbItemsRepository class after I cloned my project on my other PC.

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -6,28 +6,34 @@ using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Catalog.Controllers{
+namespace Catalog.Controllers
+{
     //Get/items
     [ApiController]
     [Route("[controller]")]
-    public class ItemsController : ControllerBase{
+    public class ItemsController : ControllerBase
+    {
         private readonly IItemsRepository repository;
-        public ItemsController(IItemsRepository repository){
+        public ItemsController(IItemsRepository repository)
+        {
             this.repository = repository;
         }
 
         //GET/items
         [HttpGet]
-        public IEnumerable<ItemDto> GetItems(){
+        public IEnumerable<ItemDto> GetItems()
+        {
             var items = repository.GetItems().Select(item => item.AsDto());
             return items;
         }
 
         //GET/items/{id}
         [HttpGet("{id}")]
-        public ActionResult<ItemDto> GetItem(Guid id){
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
             var item = repository.GetItem(id);
-            if(item is null){
+            if (item is null)
+            {
                 return NotFound();
             }
             return item.AsDto();
@@ -35,8 +41,10 @@ namespace Catalog.Controllers{
 
         //POST/items
         [HttpPost]
-        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto){
-            Item item = new(){
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
                 Id = Guid.NewGuid(),
                 Name = itemDto.Name,
                 Price = itemDto.Price,
@@ -44,19 +52,22 @@ namespace Catalog.Controllers{
             };
 
             repository.CreateItem(item);
-            return CreatedAtAction(nameof(GetItem),new {id = item.Id},item.AsDto());
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
 
         //PUT/items/{id}
         [HttpPut("{id}")]
-        public ActionResult UpdateItem(Guid id,UpdateItemDto itemDto){
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
             var existingItem = repository.GetItem(id);
 
-            if(existingItem is null){
+            if (existingItem is null)
+            {
                 return NotFound();
             }
 
-            Item updatedItem = existingItem with {
+            Item updatedItem = existingItem with
+            {
                 Name = itemDto.Name,
                 Price = itemDto.Price
             };
@@ -68,10 +79,12 @@ namespace Catalog.Controllers{
 
         //DELETE/items/{id}
         [HttpDelete("{id}")]
-        public ActionResult DeleteItem(Guid id){
+        public ActionResult DeleteItem(Guid id)
+        {
             var existingItem = repository.GetItem(id);
 
-            if(existingItem is null){
+            if (existingItem is null)
+            {
                 return NotFound();
             }
 
@@ -79,6 +92,6 @@ namespace Catalog.Controllers{
 
             return NoContent();
         }
-    
+
     }
 }
---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private const string databaseName = "catalog";
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> itemsCollection;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+        }
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
