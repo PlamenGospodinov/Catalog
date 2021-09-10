3d13c516d7dcff10f19b459981be8d1787352d56
Пламен Господинов <plamko3@abv.bg>
2021-09-10 18:10:59 +0300

    Added health checks for the API.

--- Startup.cs
+++ Startup.cs
@@ -50,6 +50,8 @@ namespace Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +73,7 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
